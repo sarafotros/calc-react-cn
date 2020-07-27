20242d4d26117ead4fe71dce999768cb6a079c3d
sarafotros <sarafotros@gmail.com>
2020-07-27 10:30:03 +0100

    Updates

---
+++ precache-manifest.e59fecd661ff8103d38a83205347f691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f5d0a79b9262400576fa1c8a28ffeb",
+    "url": "/sfotros/calculator-react/index.html"
+  },
+  {
+    "revision": "2c5b3ceabaf5d4449db9",
+    "url": "/sfotros/calculator-react/static/css/main.7fa15487.chunk.css"
+  },
+  {
+    "revision": "a1e165aa7b35c4961952",
+    "url": "/sfotros/calculator-react/static/js/2.2179ff43.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/sfotros/calculator-react/static/js/2.2179ff43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5b3ceabaf5d4449db9",
+    "url": "/sfotros/calculator-react/static/js/main.b093aac8.chunk.js"
+  },
+  {
+    "revision": "74a7fc146de2888531c5",
+    "url": "/sfotros/calculator-react/static/js/runtime-main.5be0fdd5.js"
+  }
+]);
